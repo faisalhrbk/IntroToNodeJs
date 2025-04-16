3081ffc2dfebc5fb05fd7f4a437ac20bafce9599
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-16 20:51:23 +0500

    send first h1 to client booyah

--- Lecture 4 - req and res/app.js
+++ Lecture 4 - req and res/app.js
@@ -2,10 +2,14 @@ const http = require("http");
 
 http
 	.createServer((req, res) => {
-		console.log(req.url, req.method, );
-		res.setHeader('Content-Type', 'text/html');
-		
-		
+		console.log(req.url);
+		res.setHeader("Content-Type", "text/html");
+		res.write("<html>");
+		res.write("<head><title>My Page</title></head>");
+		res.write("<body><h1>Hello World</h1></body>");
+		res.write("</html>");
+
+		res.end();
 	})
 	.listen(3001, () => {
 		console.log("Server running on http://localhost:3001");
