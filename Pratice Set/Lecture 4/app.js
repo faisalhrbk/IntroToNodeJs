3cae57c9b8f2a8ef5fba069bdd041a1af5bf0396
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-16 22:02:27 +0500

    added the home page for navigations

--- Pratice Set/Lecture 4/app.js
+++ Pratice Set/Lecture 4/app.js
@@ -5,7 +5,12 @@ http
 		res.setHeader("Content-Type", "text/html");
 
 		if (req.url.toLowerCase() === "/") {
-			res.write("<h1>Home Page</h1>");
+			res.write("<h1>Myntra  Page</h1>");
+			res.write('<a href="/men">Men</a>');
+			res.write('<a href="/women">Women</a>');
+			res.write('<a href="kids">Kids</a>');
+			res.write('<a href="cart">Cart</a>');
+		
 		} else if (req.url.toLowerCase() === "/men") {
 			res.write("<h1>Men Page</h1>");
 		} else if (req.url.toLowerCase() === "/women") {
@@ -18,7 +23,7 @@ http
 			res.write("<h1>404 Page Not Found</h1>");
 		}
 
-		res.end(); 
+		res.end();
 	})
 	.listen(3000, () => {
 		console.log("Server running at http://localhost:3000");
