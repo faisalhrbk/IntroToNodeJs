da9417b8a055ce7ec84f7d1b2ee12226210bf06e
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-16 22:28:46 +0500

    studied about buffers chunks streams and more an how to handle them

---
+++ Lecture 5 - Parsing Request/app.js
@@ -0,0 +1,39 @@
+// this chapter is more of theory!
+const http = require("http");
+const fs = require("fs");
+
+http
+	.createServer((req, res) => {
+		console.log(req.url);
+		res.setHeader("Content-Type", "text/html");
+
+		if (req.url === "/") {
+			res.write("<html>");
+			res.write("<head><title>My Page</title></head>");
+			res.write("<body>");
+			res.write('<form action="/submit", method="post">');
+			res.write(
+				'<input type="text" name="name" placeholder="enter your name">'
+			);
+			res.write(
+				'<input type="text" name="email" placeholder="enter YOUr email">'
+			);
+			res.write('<input type="submit" value="submit">');
+			res.write("</form>");
+
+			res.write("</body>");
+			res.write("</html>");
+		} else if (req.url === "/submit" && req.method === "POST") {
+			const body = [];
+			req.on("data", (chunk) => body.push(chunk));
+			req.on("end", () => {
+				const parsedBody = Buffer.concat(body).toString();
+				console.log(parsedBody);
+			});
+		}
+
+		res.end();
+	})
+	.listen(3001, () => {
+		console.log("Server running on http://localhost:3001");
+	});
