e53b2740186a57842958cf66202fa73985abe0c3
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-16 21:00:22 +0500

    showing daata to users with different routes

--- Lecture 4 - req and res/app.js
+++ Lecture 4 - req and res/app.js
@@ -4,10 +4,20 @@ http
 	.createServer((req, res) => {
 		console.log(req.url);
 		res.setHeader("Content-Type", "text/html");
-		res.write("<html>");
-		res.write("<head><title>My Page</title></head>");
-		res.write("<body><h1>Hello World</h1></body>");
-		res.write("</html>");
+
+		if (req.url === "/") {
+				res.write("<html>");
+				res.write("<head><title>My Page</title></head>");
+				res.write("<body><h1>hello / route</h1></body>");
+				res.write("</html>");
+		} else if (req.url === "/products") {
+				res.write("<html>");
+				res.write("<head><title>My Page</title></head>");
+				res.write("<body><h1>Hello  /Products route</h1></body>");
+				res.write("</html>");
+
+		}
+	
 
 		res.end();
 	})
