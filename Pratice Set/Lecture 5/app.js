d605ec63a61858506b4d3eb77cb2a1811f23ade0
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-16 23:36:24 +0500

    started working on pratice set

---
+++ Pratice Set/Lecture 5/app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const requestHandler = require("./handler");
+
+const server = http.createServer(requestHandler);
+
+server.listen(3000, () =>
+	console.log("server running on address http://localhost:3000")
+);
