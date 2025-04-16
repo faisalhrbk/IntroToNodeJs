4d46df57f5a2d0e267192812f87cb612c5aeb39d
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-16 22:51:30 +0500

    now saving user details in file

--- Lecture 5 - Parsing Request/app.js
+++ Lecture 5 - Parsing Request/app.js
@@ -11,7 +11,7 @@ http
 			res.write("<html>");
 			res.write("<head><title>My Page</title></head>");
 			res.write("<body>");
-			res.write('<form action="/submit", method="post">');
+			res.write('<form action="/submit", method="POST">');
 			res.write(
 				'<input type="text" name="name" placeholder="enter your name">'
 			);
@@ -23,20 +23,25 @@ http
 
 			res.write("</body>");
 			res.write("</html>");
-		} else if (req.url === "/submit" && req.method === "POST") {
+		} else if (req.url === "/submit" && req.method == "POST") {
 			const body = [];
 			req.on("data", (chunk) => body.push(chunk));
 			req.on("end", () => {
 				const parsedBody = Buffer.concat(body).toString();
 				console.log(parsedBody);
 				// Parsing Request;
-				const bodyObj = {};
+				// const bodyObj = {};
+				const params = new URLSearchParams(parsedBody);
+				// for (const [key, val] of params.entries()) {
+				// 	bodyObj[key] = val;
+				// }
 
-				const params = new  URLSearchParams(parsedBody);
-				for (const [key, val] of params.entries()) {
-					bodyObj[key] = val;
-				}
+                //todo easiest way to do this
+                const bodyObj = Object.fromEntries(params);
+				console.log(bodyObj);
+                 fs.writeFileSync("user.txt", JSON.stringify(bodyObj));
 			});
+           
 		}
 
 		res.end();
