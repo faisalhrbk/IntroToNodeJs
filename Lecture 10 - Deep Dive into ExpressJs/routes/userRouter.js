2c55c70ff0eec2d7a9741136c670346290283130
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-17 19:03:43 +0500

    res.sendFile(path.join(__dirname, "../", "views", "home.html")); });  understand about this

--- Lecture 10 - Deep Dive into ExpressJs/routes/userRouter.js
+++ Lecture 10 - Deep Dive into ExpressJs/routes/userRouter.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const userRouter = express.Router();
 
 userRouter.get("/", (req, res, next) => {
-	res.send(`<h1> Welcome to airbnb</h1>
-		<a href="/add-home">Add Home</a>
-		`);
+	res.sendFile(path.join(__dirname, "../", "views", "home.html"));
 });
 
 module.exports = userRouter;
