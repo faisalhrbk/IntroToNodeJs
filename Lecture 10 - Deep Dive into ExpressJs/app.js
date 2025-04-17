c8b4be33589c5ed2d953ba0a440ca16c4d48ec21
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-17 18:00:05 +0500

    getting use to it

--- Lecture 10 - Deep Dive into ExpressJs/app.js
+++ Lecture 10 - Deep Dive into ExpressJs/app.js
@@ -1,24 +1,27 @@
 //external module
 const express = require("express");
-const bodyParser = require("body-parser");
+//local modules
 
 const app = express();
+//middleware 
+app.use(express.urlencoded({ extended: true }));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use("/", (req, res, next) => {
-	console.log("came in first middleware");
-	// res.send("hello world");
-	next();
+app.get("/", (req, res, next) => {
+	res.send(`<h1> Welcome to airbnb</h1>
+		<a href="/add-home">Add Home</a>
+		`);
 });
-app.post("/submit-details", (req, res, next) => {
-	console.log("came in 2 middleware");
-	next();
+app.get("/add-home", (req, res, next) => {
+	res.send(`<h1> Register your Home Here!</h1>
+	<form action="/add-home" method="post">
+	<input type="text" name="houseName" placeholder="Enter Your House Name" />
+	<input type="submit" />
+    </form>`);
 });
-app.use("/chill", (req, res, next) => {
-	console.log("came in 3 middleware");
+app.post("/add-home", (req, res, next) => {
+	res.send(`home registered successfully </br> 
+		<a href="/">Go to Home</a> `);
 });
-
 app.listen(3000, () =>
 	console.log("server running on address http://localhost:3000")
 );
