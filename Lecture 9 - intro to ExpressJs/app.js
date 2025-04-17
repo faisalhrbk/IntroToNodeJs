20cc6ef51c6cb3d6e6783a2d9243ca1e76b8e788
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-17 16:59:06 +0500

    started to work with pratice set

--- Lecture 9 - intro to ExpressJs/app.js
+++ Lecture 9 - intro to ExpressJs/app.js
@@ -3,16 +3,16 @@ const express = require("express");
 const app = express();
 //local module
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
 	console.log("came in first middleware");
-	res.send("hello world");
+	// res.send("hello world");
 	next();
 });
-app.use((req, res, next) => {
+app.post("/submit-details",(req, res, next) => {
 	console.log("came in 2 middleware");
 	next();
 });
-app.use((req, res, next) => {
+app.use('/chill',(req, res, next) => {
 	console.log("came in 3 middleware");
 });
 
