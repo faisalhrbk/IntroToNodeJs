61ed51500591c8e6234d2c6985c4e27bc38a9cb4
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-17 16:24:59 +0500

    added nodemon start command to packagejson file

--- Lecture 9 - intro to ExpressJs/app.js
+++ Lecture 9 - intro to ExpressJs/app.js
@@ -5,8 +5,15 @@ const express = require("express");
 const app = express();
 //local module
 
-app.use(()=>{
-
+app.use((req, res, next) => {
+	console.log("came in first middleware");
+    next();
+});
+app.use((req, res, next) => {
+	console.log("came in 2 middleware");
+});
+app.use((req, res, next) => {
+	console.log("came in 3 middleware");
 });
 http
 	.createServer(app)
