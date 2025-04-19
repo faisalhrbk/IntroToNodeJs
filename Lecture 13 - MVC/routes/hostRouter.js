83cfdfd462c55f94d8b9c5f6ec05aaa6a0520eb2
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-19 14:02:33 +0500

    ahhh shit here we go again another day of coding toady ill learn about controllres spearate the logic from routes

--- Lecture 13 - MVC/routes/hostRouter.js
+++ Lecture 13 - MVC/routes/hostRouter.js
@@ -3,16 +3,16 @@ const path = require("path");
 const hostRouter = express.Router();
 const view = require("../utils/viewsPath");
 
-hostRouter.get("/add-home", (req, res, next) => {
-
-	res.render('addHome', {pageTitle: 'airbnb Home', currentPage: 'addHome'});
-});
+hostRouter.get("/add-home", );
 
 const registeredHomes = [];
 hostRouter.post("/add-home", (req, res, next) => {
-	console.log(req.body)
-	registeredHomes.push(req.body)
-	res.render("homeAdded", { pageTitle: "airbnb home", currentPage: "homeAdded" });
+	console.log(req.body);
+	registeredHomes.push(req.body);
+	res.render("homeAdded", {
+		pageTitle: "airbnb home",
+		currentPage: "homeAdded",
+	});
 });
 exports.hostRouter = hostRouter;
 exports.registeredHomes = registeredHomes;
