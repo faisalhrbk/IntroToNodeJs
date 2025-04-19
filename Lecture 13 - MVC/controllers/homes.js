15df5e8d3ead8c510f9573e3ed9b09b5886f5b1e
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-19 15:43:38 +0500

    fixed nodemon causing problems

--- Lecture 13 - MVC/controllers/homes.js
+++ Lecture 13 - MVC/controllers/homes.js
@@ -1,4 +1,4 @@
-
+const Home = require("../models/home");
 
 exports.getAddHome = (req, res, next) => {
 	res.render("addHome", { pageTitle: "airbnb Home", currentPage: "addHome" });
@@ -6,13 +6,16 @@ exports.getAddHome = (req, res, next) => {
 
 exports.postAddHome = (req, res, next) => {
 	console.log(req.body);
-	registeredHomes.push(req.body);
+	const { houseName, price, location, rating, photoUrl } = req.body;
+	const home = new Home(houseName, price, location, rating, photoUrl);
+	home.save();
 	res.render("homeAdded", {
 		pageTitle: "airbnb home",
 		currentPage: "homeAdded",
 	});
 };
 exports.getHomes = (req, res, next) => {
+    const registeredHomes = Home.fetchAll();
 	console.log(registeredHomes);
 	res.render("home", {
 		registeredHomes,
--- Lecture 13 - MVC/models/home.js
+++ Lecture 13 - MVC/models/home.js
@@ -1,5 +1,6 @@
-const { proppatch } = require("../routes/userRouter");
-
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../utils/rootDir");
 const registeredHomes = [];
 module.exports = class Home {
 	constructor(houseName, price, location, rating, photoUrl) {
@@ -8,5 +9,15 @@ module.exports = class Home {
 		this.location = location;
 		this.rating = rating;
 		this.photoUrl = photoUrl;
+	}
+	save() {
+		registeredHomes.push(this);
+		const homeDataPath = path.join(rootDir, "data", "homes.json");
+		fs.writeFile(homeDataPath, JSON.stringify(registeredHomes), (error) =>
+			console.log("File write concluded", error)
+		);
+	}
+	static fetchAll() {
+		return registeredHomes;
 	}
 };
