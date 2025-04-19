1cfa1be89fc58ae75e8260374af0476a9c646231
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-19 16:38:48 +0500

    added folder and fixed partials path

--- Lecture 13 - MVC/controllers/homes.js
+++ Lecture 13 - MVC/controllers/homes.js
@@ -1,7 +1,10 @@
 const Home = require("../models/home");
 
 exports.getAddHome = (req, res, next) => {
-	res.render("addHome", { pageTitle: "airbnb Home", currentPage: "addHome" });
+	res.render("host/addHome", {
+		pageTitle: "airbnb Home",
+		currentPage: "addHome",
+	});
 };
 
 exports.postAddHome = (req, res, next) => {
@@ -9,18 +12,17 @@ exports.postAddHome = (req, res, next) => {
 	const { houseName, price, location, rating, photoUrl } = req.body;
 	const home = new Home(houseName, price, location, rating, photoUrl);
 	home.save();
-	res.render("homeAdded", {
+	res.render("host/homeAdded", {
 		pageTitle: "airbnb home",
 		currentPage: "homeAdded",
 	});
 };
 exports.getHomes = (req, res, next) => {
 	Home.fetchAll((registeredHomes) => {
-		res.render("home", {
+		res.render("store/homeList", {
 			registeredHomes,
 			pageTitle: "airbnb home",
 			currentPage: "home",
 		});
 	});
-
 };
