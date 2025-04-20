b5e4e4cd5606965f85486f3abd557fe8976fcb14
faisalhrbk <2023-kiu-bs-3511@students.kiu.edu.pk>
2025-04-20 22:22:05 +0500

    added input field id

--- Lecture 14 - Dynamic Path/routes/hostRouter.js
+++ Lecture 14 - Dynamic Path/routes/hostRouter.js
@@ -12,6 +12,6 @@ hostRouter.get("/add-home", hostController.getAddHome);
 hostRouter.post("/add-home", hostController.postAddHome);
 hostRouter.get("/home-list", hostController.getHostHomes);
 hostRouter.get("/edit-home/:homeId", hostController.getEditHome);
-
+// hostRouter.post("/edit-home", hostController.postEditHome);
 
 module.exports = hostRouter;
