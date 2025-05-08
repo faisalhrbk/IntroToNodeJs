52a64290598e70064bfa798828c8f6c0ac930872
faisalhrbk <faisalhrbk@outlook.com>
2025-05-08 22:08:59 +0500

    i get it bruhh do;t need to reuseme

--- 111-npm-packages/express-validator/middlewares/registerValidator.js
+++ 111-npm-packages/express-validator/middlewares/registerValidator.js
@@ -1,18 +1,40 @@
 import { check, validationResult } from "express-validator";
 
-const registerValidator = [
+export const addDoctorValidator = [
 	check("name")
-		.trim()
-		.escape()
 		.notEmpty()
-		.withMessage("Name is Required")
+		.withMessage("Name required")
 		.isLength({ min: 3 })
-		.withMessage("name must be atleast 3 char long")
-		.isLength({ max: 20 })
-		.withMessage("name must be less than 20 chars")
-		.matches([a - zA - z])
-		.withMessage("name should not include numbers"),
-	check(),
+		.withMessage("Name must be 3+ chars")
+		.trim() // Remove whitespace
+		.escape(), // Prevent XSS
+	check("email")
+		.notEmpty()
+		.withMessage("Email required")
+		.isEmail()
+		.withMessage("Invalid email")
+		.normalizeEmail(), // Standardize email
+	check("password")
+		.notEmpty()
+		.withMessage("Password required")
+		.isLength({ min: 6 })
+		.withMessage("Password must be 6+ chars")
+		.matches(/[A-Za-z]/)
+		.withMessage("Password needs a letter")
+		.matches(/[0-9]/)
+		.withMessage("Password needs a number"),
+	check("imageFile").custom((value, { req }) => {
+		if (!req.file) throw new Error("Image required");
+		if (!["image/jpeg", "image/png", "image/gif"].includes(req.file.mimetype)) {
+			throw new Error("Only JPEG, PNG, GIF allowed");
+		}
+		return true;
+	}),
 ];
 
-export default registerValidator;
+export const validate = (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty())
+		return res.status(400).json({ errors: errors.array() });
+	next();
+};
